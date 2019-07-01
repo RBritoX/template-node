cb53e9c061a177ac314ae7d10544fd781fd6a299
RBritoX <raphaellbrito@gmail.com>
2019-07-01 01:11:20 -0300

    template-node

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: '', // banco => postgres, mysql...
+  host: 'localhost',
+  username: '',
+  password: '',
+  database: '',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
